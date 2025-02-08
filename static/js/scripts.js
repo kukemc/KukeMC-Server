a3d72a6bd911e63a639258650ff8f5aa6d8192f3
kukemc <2464952636@qq.com>
2025-02-08 23:18:26 +0800

    fix: back-to-top

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -342,20 +342,6 @@
         }
         $("#pmsgSubmit").removeClass().addClass(msgClasses).text(msg);
     }
-    
-
-    /* Back To Top Button */
-    // create the back to top button
-    $('body').prepend('<a href="body" class="back-to-top page-scroll">Back to Top</a>');
-    var amountScrolled = 700;
-    $(window).scroll(function() {
-        if ($(window).scrollTop() > amountScrolled) {
-            $('a.back-to-top').fadeIn('500');
-        } else {
-            $('a.back-to-top').fadeOut('500');
-        }
-    });
-
 
 	/* Removes Long Focus On Buttons */
 	$(".button, a, button").mouseup(function() {
