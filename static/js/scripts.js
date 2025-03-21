1606f726f185b6c0a79491e7496cbe2eeb18340e
kukemc <2464952636@qq.com>
2025-03-21 18:30:34 +0800

    fix: 加载动画不隐藏的BUG

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -106,7 +106,7 @@
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
         if (!$(this).parent().hasClass('dropdown'))
-            $(".navbar-collapse").collapse('');
+            $(".navbar-collapse").collapse('hide');
     });
 
     /* Rotating Text - Morphtext */
